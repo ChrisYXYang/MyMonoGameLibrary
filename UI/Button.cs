53c809559aaf8d55b32865604d14cfec195cc549
ChrisYXYang <chrisyang2004@outlook.com>
2025-08-04 15:09:59 -0500

    button

---
+++ UI/Button.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using MyMonoGameLibrary.Graphics;
+using MyMonoGameLibrary.Input;
+using MyMonoGameLibrary.Scenes;
+
+namespace MyMonoGameLibrary.UI;
+
+// this class contains base funcionalities for a button (might be useless we'll see)
+public abstract class Button : BehaviorComponent
+{
+    private UISprite _sr;
+    public Sprite Normal;
+    public Sprite Hover;
+    public Sprite Press;
+
+    private bool _pressed = false;
+    private bool _hover = false;
+
+    public Button(Sprite normal, Sprite hover, Sprite press)
+    {
+        Normal = normal;
+        Hover = hover;
+        Press = press;
+    }
+
+    public Button(Sprite normal, Sprite hover)
+    {
+        Normal = normal;
+        Hover = hover;
+        Press = normal;
+    }
+
+    public override void Start()
+    {
+        _sr = GetComponent<UISprite>();
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        if (Collisions.MouseInUICollider(Parent.Collider))
+        {
+            if (!InputManager.Mouse.IsButtonDown(MouseButton.Left))
+            {
+                _hover = true;
+            }
+            else
+            {
+                _hover = false;
+            }
+
+            if (InputManager.Mouse.WasButtonJustPressed(MouseButton.Left))
+            {
+                _pressed = true;
+            }
+
+            if (InputManager.Mouse.WasButtonJustReleased(MouseButton.Left) && _pressed)
+            {
+                Clicked();
+                _hover = false;
+                _pressed = false;
+                _sr.Sprite = Normal;
+            }
+        }
+        else
+        {
+            _sr.Sprite = Normal;
+            _pressed = false;
+            _hover = false;
+        }
+
+        if (_hover)
+        {
+            _sr.Sprite = Hover;
+        }
+
+        if (_pressed)
+        {
+            _sr.Sprite = Press;
+
+        }
+    }
+
+    public abstract void Clicked();
+}
--- UI/UICamera.cs
+++ UI/UICamera.cs
@@ -11,7 +11,7 @@ public static class UICamera
 {
     // scale to draw
     public static float Scale { get; set; } = Camera.PixelScale;
-    
+
     // draw an UI element
     //
     // param: ui - the ui element
@@ -28,7 +28,7 @@ public static class UICamera
         {
             if (!text.IsVisible)
                 return;
-            
+
             Core.SpriteBatch.DrawString
                 (
                     text.Font,
@@ -44,7 +44,7 @@ public static class UICamera
         }
         else if (ui.Renderer is UISprite sprite)
         {
-            if (!sprite.IsVisible)
+            if (!sprite.IsVisible || sprite.Sprite == null)
                 return;
 
             Core.SpriteBatch.Draw
